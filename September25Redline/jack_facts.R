a21742a5a116af54506262c274114b1cb25518f0
lbeatty <lauren.k.beatty@gmail.com>
2025-12-02 10:39:19 -0500

    add plot

--- September25Redline/jack_facts.R
+++ September25Redline/jack_facts.R
@@ -1,6 +1,6 @@
 library(data.table)
-library(lubridate)  
-library(openxlsx)    
+library(lubridate)
+library(openxlsx)
 library(ggplot2)
 library(scales)
 library(kableExtra)
@@ -48,20 +48,34 @@ well_counts <- wellbores[!is.na(FirstCompletionDate),
 well_counts[order(year), cum_wells := cumsum(n_wells), by = state]
 
 #year from when cross threshold
-threshold=10000
+threshold=5000
 well_counts[,geq_threshold:=cum_wells>threshold]
 well_counts[geq_threshold==T,min_threshold_yr:=min(year), by=state]
 well_counts[,yrs_since_threshold:=year-min_threshold_yr]
 
-ggplot(well_counts[state%in%c('Utah', 'Colorado', 'New Mexico')], aes(x = yrs_since_threshold, y = cum_wells, color = state)) +
-  geom_line() +
+ggplot(well_counts[state %in% c("Utah", "Colorado", "New Mexico")], aes(x = yrs_since_threshold, y = cum_wells, color = state)) +
+  geom_line(lwd = 1.5) +
   theme_bw() +
   labs(
-    x = "First Completion Date",
-    y = "Cumulative Wells (First Completion ≤ x)",
-    title = "Cumulative Wells by First Completion Date and State",
+    x = "Years Since Passing 5,000 Wells",
+    y = "Cumulative Wells",
     color = "State"
+  ) +
+  scale_y_continuous(labels = comma) +
+  theme(
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 12),
+    legend.title = element_text(size = 14),
+    legend.text = element_text(size = 12)
   )
+
+
+ggsave(
+  filename = file.path("UtahDNRAnalytics/September25Redline/Figs/Threshold_Timeseries.png"),
+  width = 6,
+  height = 4,
+  dpi = 300
+)
 
 ## break out by basins
 ut_wells = wellbores[state=="Utah"]
@@ -83,7 +97,7 @@ quant_table <- ut_wells[, .(
   gas_p50  = quantile(Last12MonthGasProduction_MCF, 0.50, na.rm = TRUE),
   gas_p75  = quantile(Last12MonthGasProduction_MCF, 0.75, na.rm = TRUE),
   gas_p90  = quantile(Last12MonthGasProduction_MCF, 0.90, na.rm = TRUE),
-  
+
   oil_p10  = quantile(Last12MonthOilProduction_BBL, 0.10, na.rm = TRUE),
   oil_p25  = quantile(Last12MonthOilProduction_BBL, 0.25, na.rm = TRUE),
   oil_p50  = quantile(Last12MonthOilProduction_BBL, 0.50, na.rm = TRUE),
